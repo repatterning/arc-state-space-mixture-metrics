2d7e6d07ef63ff13c9f14fb96f181d7b10c700d4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-09-28 22:48:51 +0100

    Add src/cartography/data.py

---
+++ src/cartography/data.py
@@ -0,0 +1,16 @@
+
+import geopandas
+
+
+class Data:
+
+    def __init__(self, care: geopandas.GeoDataFrame):
+        """
+
+        :param care:
+        """
+
+        self.__care = care
+
+    def exc(self):
+        pass
