d687492accad0c0e2127f4cd990a63815173322f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-09-28 23:22:55 +0100

    Update src/cartography/data.py: In Progress

--- src/cartography/data.py
+++ src/cartography/data.py
@@ -1,5 +1,8 @@
+"""Module cartography/data.py"""
+import logging
 
 import geopandas
+import pandas as pd
 
 
 class Data:
@@ -10,7 +13,38 @@ class Data:
         :param care:
         """
 
-        self.__care = care
+        self.__care = self.__get_care(care=care.copy())
 
-    def exc(self):
-        pass
+        # fields
+        self.__f_care = ['catchment_id', 'catchment_name', 'focus', 'latitude', 'longitude', 'organisation',
+                         'town', 'local_authority']
+        self.__f_risks = ['catchment_id', 'catchment_name', 'focus', 'latitude', 'longitude', 'station_name',
+                          'latest', 'maximum', 'minimum', 'median', 'ending', 'river_name']
+
+    @staticmethod
+    def __get_care(care):
+        """
+
+        :param care:
+        :return:
+        """
+
+        care['latitude'] = care.geometry.apply(lambda k: k.y)
+        care['longitude'] = care.geometry.apply(lambda k: k.x)
+        care['focus'] = 'elders'
+
+        return care
+
+    def exc(self, risks: pd.DataFrame):
+        """
+
+        :param risks:
+        :return:
+        """
+
+        risks['focus'] = 'gauge'
+
+        # Concatenating
+        data = pd.concat([self.__care[self.__f_care], risks[self.__f_risks]], axis=0, ignore_index=True)
+
+        return data
