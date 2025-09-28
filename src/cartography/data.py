b357068b37c5a55ccbb9c812a8c1206218a17ba4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-09-29 00:55:40 +0100

    Update src/cartography/data.py

--- src/cartography/data.py
+++ src/cartography/data.py
@@ -18,12 +18,12 @@ class Data:
 
         # fields
         self.__f_care = ['catchment_id', 'catchment_name', 'focus', 'latitude', 'longitude', 'organisation',
-                         'town', 'local_authority']
+                         'town', 'local_authority', 'geometry']
         self.__f_risks = ['catchment_id', 'catchment_name', 'focus', 'latitude', 'longitude', 'station_name',
-                          'latest', 'maximum', 'minimum', 'median', 'ending', 'river_name']
+                          'latest', 'maximum', 'minimum', 'median', 'ending', 'river_name', 'geometry']
 
     @staticmethod
-    def __get_care(care: geopandas.GeoDataFrame):
+    def __get_care(care: geopandas.GeoDataFrame) -> geopandas.GeoDataFrame:
         """
 
         :param care: Care home frame
@@ -36,7 +36,7 @@ class Data:
 
         return care
 
-    def exc(self, risks: pd.DataFrame):
+    def exc(self, risks: geopandas.GeoDataFrame) -> geopandas.GeoDataFrame:
         """
 
         :param risks: A frame of river level change rates, etc.
